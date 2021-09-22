fa96719b02dc64bf0074f5a901000d790223ab1e
anitta12345 <91114914+anitta12345@users.noreply.github.com>
2021-09-22 14:08:29 +0530

    Delete program-4.py

--- program-4.py
+++
@@ -1,6 +0,0 @@
-input=[4,5,3,2]
-filter(lambda x: x%4==0, input)
-list(filter(lambda x: x%4==0, input))
-len(list(filter(lambda x: x%4==0, input)))
-output={i:len(list(filter(lambda x: x%i==0, input))) for i in input}
-print(output)
